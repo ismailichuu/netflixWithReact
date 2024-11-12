c21f600fae48c85c5ba20cb55f32b464795e80de
Ismail ichu <ismailichu2001@gmail.com>
2024-11-12 21:55:13 +0530

    Completed all Design works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
