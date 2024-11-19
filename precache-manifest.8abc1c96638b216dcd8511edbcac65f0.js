319c0bf0631c8e0288ad0fcbd8d72627d8ece568
Ismail ichu <ismailichu2001@gmail.com>
2024-11-19 15:32:53 +0530

    Updates

---
+++ precache-manifest.8abc1c96638b216dcd8511edbcac65f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cc761197ac3f542b203cc2b625b468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa54ce7f307955b07336",
+    "url": "/static/css/main.f020819a.chunk.css"
+  },
+  {
+    "revision": "06a14b2ec6eae751d77a",
+    "url": "/static/js/2.41252666.chunk.js"
+  },
+  {
+    "revision": "5a4886f14037d20c58d1df2b9580f715",
+    "url": "/static/js/2.41252666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa54ce7f307955b07336",
+    "url": "/static/js/main.9a546847.chunk.js"
+  },
+  {
+    "revision": "551370adbb1837c3f01f",
+    "url": "/static/js/runtime-main.3ecbf3fa.js"
+  }
+]);
